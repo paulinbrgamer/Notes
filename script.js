0a13ecd4a63bdfddf6b86fac3ce1cb32af459e22
oPaulin <pauloferadosgames@gmail.com>
2024-09-29 14:17:16 -0300

    finalizando a abertura do menu hambuerguer

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function  abrirMenu(){
     console.log('click')
     var menu = document.getElementById('menu')
     menu.classList.toggle('menuextendido')
+    document.getElementById('ant').classList.toggle('delete')
+    document.getElementById('hb-button').classList.toggle('border')
 }
