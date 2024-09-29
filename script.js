824935627aaf628bebd6e61e476964e2879c9747
oPaulin <pauloferadosgames@gmail.com>
2024-09-29 17:39:00 -0300

    adicionada a funcionalidade de selecionar uma anotação

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-var allAnotations = [{nome: 'Uepa',task:[]}]
-
-
-
-
-
-
-
-
+//varivel que guarda todos os objetos anotaçoes
+var allAnotations = []
 desenharAnotacoes()
 
 
+
+
+
+function selectAnotacao(id){
+    toggleMenu()
+    var title = document.getElementById('title')
+    title.innerText = allAnotations[id].nome
+}
+//função que desenha as anotaçoes
 function desenharAnotacoes(){
     let update = ''
     
     var container = document.getElementById('container-anotacoes')
-    allAnotations.forEach(obj=>{
-        update =  update+`<button><p>${obj.nome}</p> </button>`
+    allAnotations.forEach((obj,index)=>{
+        update =  update+`<button onclick="selectAnotacao(${index})"><p>${obj.nome}</p> </button>`
         container.innerHTML = update
     })
 }
-
 //interação do hambuerguer
 function  toggleMenu(){
     var menu = document.getElementById('menu')
@@ -27,11 +28,12 @@ function  toggleMenu(){
     document.getElementById('ant').classList.toggle('delete')
     document.getElementById('hb-button').classList.toggle('border')
 }
+//adicionar anotação
 function addAnotacao(){
     var barra = document.getElementById('Anotation-name')
     var object = {nome: barra.value,task:[]}
     barra.value = ''
     allAnotations.push(object)
-    console.log(allAnotations)
     desenharAnotacoes()
+    console.log(allAnotations)
 }
