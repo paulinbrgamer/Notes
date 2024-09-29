c240bc5f948dc48aa29eff1be48c73be41dafb85
oPaulin <pauloferadosgames@gmail.com>
2024-09-29 17:25:52 -0300

    Adicionado função de atualizar a lista de tarefas e de adicionar tarefas

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var allAnotations = [{nome: 'Uepa',task:[]}]
+
+
+
+
+
+
+
+
+desenharAnotacoes()
+
+
+function desenharAnotacoes(){
+    let update = ''
+    
+    var container = document.getElementById('container-anotacoes')
+    allAnotations.forEach(obj=>{
+        update =  update+`<button><p>${obj.nome}</p> </button>`
+        container.innerHTML = update
+    })
+}
+
 //interação do hambuerguer
 function  toggleMenu(){
     var menu = document.getElementById('menu')
@@ -6,15 +28,10 @@ function  toggleMenu(){
     document.getElementById('hb-button').classList.toggle('border')
 }
 function addAnotacao(){
-    console.log('adicionar')
+    var barra = document.getElementById('Anotation-name')
+    var object = {nome: barra.value,task:[]}
+    barra.value = ''
+    allAnotations.push(object)
+    console.log(allAnotations)
+    desenharAnotacoes()
 }
-
-
-
-
-
-
-
-
-//criar array de anotaçoes
-var anotacoes = [{}]
